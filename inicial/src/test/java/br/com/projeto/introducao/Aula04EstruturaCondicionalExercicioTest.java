0f5c3908c6832fa30973b318de17718a2d855e2e
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2025-12-28 10:16:01 -0300

    feat: ajustando testes
    alterei os testes para um único teste parametrizados para cálculo de taxa anual por salário

--- inicial/src/test/java/br/com/projeto/introducao/Aula04EstruturaCondicionalExercicioTest.java
+++ inicial/src/test/java/br/com/projeto/introducao/Aula04EstruturaCondicionalExercicioTest.java
@@ -1,29 +1,42 @@
 package br.com.projeto.introducao;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
-import br.com.projeto.introducao.utils.Format;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+
+@RunWith(Parameterized.class)
 public class Aula04EstruturaCondicionalExercicioTest {
-//                Bracket 1	Up to € 38.883	35.70%
-//                Bracket 2	€ 38.883 to € 77.320	37,56%
-//                Bracket 3	€ 77.320	49,50%
 
-    @Test
-    public void deveAplicarPrimeiraTaxaQuandoSalarioForMenorQue38883(){
-        double salarioAnual = 28000.321;
-        double result = Aula04EstruturaCondicionalExercicio.taxaAnualPorSalario(salarioAnual);
+    private static final double MARGEM_ERRO = 0.01;
 
-        assertThat(Format.formataNumero(result, 2), is("9996.11"));
+    @Parameterized.Parameters(name = "Salário {0} deve ter taxa {1}")
+    public static Collection<Object[]> parametrosDeTeste() {
+        return Arrays.asList(new Object[][]{
+                {28000.32, 9996.11},   // Primeira faixa
+                {38883.0, 13881.23},   // Limite primeira faixa
+                {55000.0, 20658.00},   // Segunda faixa
+                {77320.0, 29041.39},   // Limite segunda faixa
+                {98346.0, 48681.27}    // Terceira faixa
+        });
     }
 
-    @Test
-    public void deveAplicarPrimeiraTaxaQuandoSalarioForExatamente38883(){
-        double salarioAnual = 38883;
-        double result = Aula04EstruturaCondicionalExercicio.taxaAnualPorSalario(salarioAnual);
+    @Parameterized.Parameter(0)
+    public double salarioAnual;
 
-        assertThat(Format.formataNumero(result, 2), is("13881.23"));
+    @Parameterized.Parameter(1)
+    public double taxaEsperada;
+
+    @Test
+    public void deveCalcularTaxaAnualCorretamente() {
+        double taxaCalculada = Aula04EstruturaCondicionalExercicio
+                .taxaAnualPorSalario(salarioAnual);
+
+        assertThat(taxaCalculada, closeTo(taxaEsperada, MARGEM_ERRO));
     }
 }
